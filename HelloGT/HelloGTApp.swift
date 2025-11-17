6015aa1667055a73c5153c7fe785e3b33fc3ad30
sanaagada <sanaagada@gmail.com>
2025-11-16 21:34:51 -0500

    Add Firebase integration)

--- HelloGT/HelloGTApp.swift
+++ HelloGT/HelloGTApp.swift
@@ -5,14 +5,20 @@
 //  Created by Sanaa Gada on 10/30/25.
 //
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @main
 struct HelloGTApp: App {
+    init() {
+            FirebaseApp.configure()
+            print("✅ Firebase configured successfully!")
+        }
+    
     var body: some Scene {
         WindowGroup {
             LaunchView()
         }
     }
 }
-
-
